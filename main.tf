5712f374cd2e4e103d92ed58044ff9b0da250fc5
truongblx <truongblx26031999@gmail.com>
2024-06-10 00:52:15 +0700

    Managed by terraform

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@
 // Modules
 module "compute" {
   source  = "app.terraform.io/dsodiary/compute/aws"
-  version = "1.0.1"
+  version = "1.0.2"
 
   aws_region          = "ap-southeast-1"
   public_key_material = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDv4dIbgwN5rsrBNAk4nqbovlkeRI39tV+i4K3Vu31mY truongblx@dso"
@@ -17,7 +17,7 @@ module "compute" {
 
 module "networking" {
   source  = "app.terraform.io/dsodiary/networking/aws"
-  version = "1.0.1"
+  version = "1.0.2"
 
   access_ip  = "0.0.0.0/0"
   aws_region = "ap-southeast-1"
